f597b0a1fad25ab86e7021537fea605279ed4349
R Res <clonnes@hotmail.com>
2019-02-23 13:28:00 -0500

    Add project files.

---
+++ Assigment_3_Group_5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Assigment_3_Group_5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
